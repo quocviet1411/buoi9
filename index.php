0878bd7f686711438db2e8ed156b85a05ebb1fef
quocviet1411 <you@example.com>
2017-04-25 21:16:04 +0700

    abc

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Tao menu</title>
+	<link rel="stylesheet" href="">
+</head>
+<style type="text/css" media="screen">
+.menu ul {
+  background: #FFCC33;
+  list-style-type: none;
+  text-align: center;
+  height: 40px;
+	}
+img{
+		width: 40px;
+		height:40px;
+		float: left;
+	}
+.menu li {
+  color: #f1f1f1;
+  float: left;
+  width: 120px;
+  height: 40px;
+  line-height: 40px;
+  background-color: #FFCC11;
+}
+</style>
+<body>
+	<div class="menu">
+		<ul>
+			<li>Thoi su</li>
+			<li>kinh te vi mo</li>
+			<li>Kinh Doanh</li>
+			<li>Cong nghe</li>
+			<li>Song</li>
+		</ul>
+	</div>
+</body>
+</html>
